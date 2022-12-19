f735c1ff8cae6ca55ff80ca223fbb573e7177f38
Armine Zakaryan <armzak91@gmail.com>
2022-12-19 15:51:08 +0400

    Initial commit

---
+++ src/main/java/com/epam/cloudgantt/CloudGanttApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cloudgantt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGanttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGanttApplication.class, args);
+    }
+
+}

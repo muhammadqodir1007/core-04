e2b0a47f488b05f6aaadaebe651a5fef2a26a02a
Farrukhbek_Khabibjonov <farrukhbek_khabibjonov@epam.com>
2023-01-27 11:39:37 +0500

    EPMRDUZECG-243- developed endpoint for uploading csv

--- src/main/java/com/epam/cloudgantt/controller/CsvTemplateController.java
+++ src/main/java/com/epam/cloudgantt/controller/CsvTemplateController.java
@@ -1,16 +1,28 @@
 package com.epam.cloudgantt.controller;
 
+import com.epam.cloudgantt.entity.User;
+import com.epam.cloudgantt.payload.ApiResult;
+import com.epam.cloudgantt.payload.ProjectResponseDTO;
+import com.epam.cloudgantt.security.CurrentUser;
 import com.epam.cloudgantt.util.AppConstants;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RequestMapping(AppConstants.BASE_PATH)
 public interface CsvTemplateController {
 
+
+    String UPLOAD_PATH = AppConstants.BASE_PATH + "upload";
+
     @GetMapping("/export-template")
     void exportCSV(HttpServletResponse response) throws IOException;
 
+    @PostMapping(value = UPLOAD_PATH)
+    ApiResult<ProjectResponseDTO> uploadCSV(@RequestParam("file") MultipartFile file, @CurrentUser User user) throws Exception;
 }
